f90bf1467695139def2575dfdfca51ad12ce19e6
gandan <188059221+gandandev@users.noreply.github.com>
2025-03-16 16:15:50 +0900

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+    ignores: ['dist/**', 'node_modules/**'],
+  }
+);
